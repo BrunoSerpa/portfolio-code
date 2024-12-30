ae9db6a803fa921be8660f024ef079a75f6376fd
BrunoSerpa <serpabruno2005@gmail.com>
2024-12-30 19:26:20 -0300

    Add Home component with personal introduction

---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,21 @@
+export default function Home() {
+    return (
+        <div>
+            <div>
+                <h1>
+                    Quem sou
+                </h1>
+                <img alt="Me" />
+                <p>
+                    Meu nome é Bruno Serpa Pereira Carvalho, tenho 19 anos e sou Técnico em Desenvolvimento de Sistemas pela Escola Técnica Estadual Professora Ilza Nascimento Pintus. Atualmente, estou cursando o Tecnólogo em Desenvolvimento de Software Multiplataforma. Minha formação acadêmica e experiência prática construíram uma base sólida em tecnologias e metodologias modernas, com foco na criação de projetos funcionais e de alto desempenho.
+                </p>
+                <p>
+                    Tenho experiência em uma ampla variedade de ferramentas e linguagens de programação, como Git, GitHub, Python, TypeScript, JavaScript, e frameworks como React, React Native e Express. Além disso, possuo conhecimento em bancos de dados relacionais e não relacionais, como PostgreSQL, MySQL, SQL Server, Cassandra, MongoDB e Redis, permitindo-me desenvolver sistemas robustos e adaptáveis a diferentes demandas.
+                </p>
+                <p>
+                    Minha meta profissional é aprimorar constantemente minhas habilidades técnicas e contribuir para o desenvolvimento de projetos inovadores que agreguem valor, otimizem processos e ofereçam soluções práticas e eficazes aos usuários.
+                </p>
+            </div>
+        </div>
+    )
+}
