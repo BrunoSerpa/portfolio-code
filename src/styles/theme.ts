3334bf98bca53d71d3224d375f208e361c928bbe
BrunoSerpa <serpabruno2005@gmail.com>
2024-12-30 22:07:26 -0300

    Add styled-components and theme colors for consistent styling

---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+const textColor1 = "#182825";
+const textColor2 = "#0D201E";
+const textColor3 = "#00332E";
+const textColor4 = "#DDE4E4";
+const backgroundColor1 = "#D8EDEE";
+const backgroundColor2 = "#BAD9D4";
+const backgroundColor3 = "#175049";
+
+export { textColor1, textColor2, textColor3, textColor4, backgroundColor1, backgroundColor2, backgroundColor3 };
