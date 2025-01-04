a5ee4cc0e7786a016bf20435dde5ff3904d4f914
BrunoSerpa <serpabruno2005@gmail.com>
2025-01-04 13:41:22 -0300

    Add IconMenu SVG asset and update index.ts to include it in exports

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,8 +4,9 @@ import IconDropdown from "./IconDropdown.svg";
 import IconEmail from "./IconEmail.svg";
 import IconGithub from "./IconGithub.svg";
 import IconLinkedin from "./IconLinkedin.svg";
+import IconMenu from "./IconMenu.svg";
 import IconPhone from "./IconPhone.svg";
 import IconTheme from "./IconTheme.svg";
 import Me from "./Me.jpg";
 
-export { FlagBrazil, IconCopy, IconDropdown, IconEmail, IconGithub, IconLinkedin, IconPhone, IconTheme, Me };
+export { FlagBrazil, IconCopy, IconDropdown, IconEmail, IconGithub, IconLinkedin, IconMenu, IconPhone, IconTheme, Me };
