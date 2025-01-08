990f05208e98c41e73cf8c6cfc90cc22fdc55e9b
BrunoSerpa <serpabruno2005@gmail.com>
2025-01-08 14:10:48 -0300

    Add new SVG icons for Association, Light, Person, Team and Techs; update index.ts to export new icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,12 +1,17 @@
 import FlagBrazil from "./FlagBrazil.svg";
+import IconAssociation from "./IconAssociation.svg";
 import IconCopy from "./IconCopy.svg";
 import IconDropdown from "./IconDropdown.svg";
 import IconEmail from "./IconEmail.svg";
 import IconGithub from "./IconGithub.svg";
 import IconLinkedin from "./IconLinkedin.svg";
+import IconLight from "./IconLight.svg";
 import IconMenu from "./IconMenu.svg";
+import IconPerson from "./IconPerson.svg";
 import IconPhone from "./IconPhone.svg";
+import IconTeam from "./IconTeam.svg";
+import IconTechs from "./IconTechs.svg";
 import IconTheme from "./IconTheme.svg";
 import Me from "./Me.jpg";
 
-export { FlagBrazil, IconCopy, IconDropdown, IconEmail, IconGithub, IconLinkedin, IconMenu, IconPhone, IconTheme, Me };
+export { FlagBrazil, IconCopy, IconAssociation, IconDropdown, IconEmail, IconGithub, IconLight, IconLinkedin, IconMenu, IconPerson, IconPhone, IconTeam, IconTechs, IconTheme, Me };
