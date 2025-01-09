62b235b684e4c482049870a3d5a806cf3002dc64
BrunoSerpa <serpabruno2005@gmail.com>
2025-01-09 00:59:47 -0300

    Add language support to Header component with text localization

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,8 +5,13 @@ import { ButtonMenu, DivContact, DivDropdown, DivFlag, DivFunctions, DivHeader, 
 import { DarkMode, WhiteMode } from "../../styles";
 import ImgSVG from "../Svg";
 import Contacts from "../Contacts";
+import { textEn, textPt, TextsHeader } from "./text";
 
 export default function Header({ data, sets }: Readonly<Props>) {
+    const [showContact, setShowContact] = useState(false);
+    const [showMenu, setShowMenu] = useState(false);
+    const [texts, setTexts] = useState<TextsHeader>(textPt);
+
     const changeTheme = () => {
         if (localStorage.getItem("theme") === "default") {
             localStorage.setItem("theme", "dark-mode");
@@ -27,20 +32,19 @@ export default function Header({ data, sets }: Readonly<Props>) {
         };
     };
 
+    const toggleDropdown = () => setShowContact(!showContact);;
+    const togleMenu = () => setShowMenu(!showMenu);
+
     useEffect(() => {
         if (localStorage.getItem("theme") !== "default") sets.theme(DarkMode);
         if (localStorage.getItem("language") !== "PT") sets.language("EN");
     }, [sets]);
 
-    const [showContact, setShowContact] = useState(false);
-    const [showMenu, setShowMenu] = useState(false);
+    useEffect(() => {
+        if (data.language === "PT") setTexts(textPt)
+        else setTexts(textEn)
+    }, [data.language]);
 
-    const toggleDropdown = () => {
-        setShowContact(!showContact);
-    };
-    const togleMenu = () => {
-        setShowMenu(!showMenu);
-    };
     return (
         <>
             <DivHeader theme={data.theme}>
@@ -48,22 +52,22 @@ export default function Header({ data, sets }: Readonly<Props>) {
                     <ImgSVG src={IconMenu} height="50" width="50" stroke={data.theme.textColor4} />
                 </ButtonMenu>
                 <DivLinks theme={data.theme} showMenu={showMenu}>
-                    <LinkHeader href="/home" theme={data.theme} onClick={togleMenu}>Início</LinkHeader>
-                    <LinkHeader href="/about" theme={data.theme} onClick={togleMenu}>Sobre mim</LinkHeader>
-                    <LinkHeader href="/experience" theme={data.theme} onClick={togleMenu}>Experiência</LinkHeader>
-                    <LinkHeader href="/formation" theme={data.theme} onClick={togleMenu}>Formação</LinkHeader>
-                    <LinkHeader href="/project" theme={data.theme} onClick={togleMenu}>Projetos</LinkHeader>
+                    <LinkHeader href="/home" theme={data.theme} onClick={togleMenu}>{texts.Home}</LinkHeader>
+                    <LinkHeader href="/about" theme={data.theme} onClick={togleMenu}>{texts.About}</LinkHeader>
+                    <LinkHeader href="/experience" theme={data.theme} onClick={togleMenu}>{texts.Experience}</LinkHeader>
+                    <LinkHeader href="/formation" theme={data.theme} onClick={togleMenu}>{texts.Formation}</LinkHeader>
+                    <LinkHeader href="/project" theme={data.theme} onClick={togleMenu}>{texts.Project}</LinkHeader>
                 </DivLinks>
                 <DivFunctions>
                     <DivContact theme={data.theme} showContact={showContact} onClick={toggleDropdown}>
-                        <TextFunctions theme={data.theme} showContact={showContact}>Contato</TextFunctions>
+                        <TextFunctions theme={data.theme} showContact={showContact}>{texts.Contact}</TextFunctions>
                         <DivDropdown showContact={showContact}>
                             <ImgSVG src={IconDropdown} height="20" width="20" path={showContact ? data.theme.textColor1 : data.theme.textColor4} />
                         </DivDropdown>
                     </DivContact>
                     <DivFlag onClick={changeLanguage}>
                         <ImgSVG src={data.language === "PT" ? FlagBrazil : FlagUSA} height="50" width="50" />
-                        <TextFunctions theme={data.theme}>Português</TextFunctions>
+                        <TextFunctions theme={data.theme}>{texts.Language}</TextFunctions>
                     </DivFlag>
                     <FunctionTheme onClick={changeTheme}>
                         <ImgSVG src={IconTheme} height="50" width="50" path={data.theme.textColor4} />
---
+++ src/components/Header/text.ts
@@ -0,0 +1,29 @@
+export interface TextsHeader {
+    Home: string;
+    About: string;
+    Experience: string;
+    Formation: string;
+    Project: string;
+    Contact: string;
+    Language: string;
+};
+
+export const textPt:TextsHeader = {
+    Home: "Início",
+    About: "Sobre mim",
+    Experience: "Experiência",
+    Formation: "Formação",
+    Project: "Projetos",
+    Contact: "Contato",
+    Language: "Português"
+};
+
+export const textEn:TextsHeader = {
+    Home: "Home",
+    About: "About",
+    Experience: "Experience",
+    Formation: "Formation",
+    Project: "Projets",
+    Contact: "Contact",
+    Language: "English"
+};
