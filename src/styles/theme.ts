cab293734fd3594afc507fbfa07832a643d8f181
BrunoSerpa <serpabruno2005@gmail.com>
2025-01-09 12:21:23 -0300

    Update DarkMode theme colors for improved contrast and aesthetics

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -12,10 +12,10 @@ export const WhiteMode: Theme = {
 
 export const DarkMode: Theme = {
     backgroundColor1: "#175049",
-    backgroundColor2: "#BAD9D4",
+    backgroundColor2: "#798C89",
     backgroundColor3: "#D8EDEE",
-    textColor1: "#DDE4E4",
-    textColor2: "#00332E",
-    textColor3: "#0D201E",
-    textColor4: "#182825"
+    textColor1: "#D8EDEE",
+    textColor2: "#CCDFE0",
+    textColor3: "#92A0A1",
+    textColor4: "#020303" 
 };
